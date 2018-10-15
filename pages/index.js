c5fcfadde81289737052627e0e69c8ccc3d10280
Slava Bezgachev <bezgachev@gmail.com>
2018-10-15 11:48:50 +0600

    Dynamic routes with queries

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import React from "react";
-import MyLayout from "../components/MyLayout";
+import Layout from "../components/MyLayout.js";
+import Link from "next/link";
 
-const Index = () => {
-  return (
-    <MyLayout>
-      <p>Hello Next.js</p>
-    </MyLayout>
-  );
-};
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
-export default Index;
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
+  </Layout>
+);
